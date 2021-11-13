f066038461a4fe5292806869cf76da9ba1ac147b
Unnikrishnan Nirmala <unnikrishnan.dn@gmail.com>
2021-11-14 04:44:15 +0530

    Landing page

---
+++ src/main/java/com/primus/security/SecurityAdapter.java
@@ -0,0 +1,54 @@
+package com.primus.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@EnableWebSecurity
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DefaultAuthenticationProvider defaultAuthenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider((AuthenticationProvider)defaultAuthenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+          http.authorizeRequests().antMatchers("/scripts/support.js").permitAll()
+                  .anyRequest().authenticated().and().formLogin().and().httpBasic().and().headers().frameOptions().sameOrigin().and().csrf()
+                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().formLogin().loginPage("/bologin.html").failureUrl("/bologin.html?error=true")
+                  .loginProcessingUrl("/bologin.html").permitAll().failureHandler(new AuthenticationFailureHandler() {
+              @Override
+              public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                  UrlPathHelper urlPathHelper =new UrlPathHelper();
+                  String contextPath = urlPathHelper.getContextPath(request);
+                  response.sendRedirect(contextPath + "/bologin.html?error=true");
+              }
+          });
+
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("https://maxcdn.bootstrapcdn.com/**","https://stackpath.bootstrapcdn.com/**",
+                "**/scripts/support.js","https://cdnjs.cloudflare.com/**","https://fonts.googleapis.com/**",
+                "https://ajax.googleapis.com/**");
+    }
+}
